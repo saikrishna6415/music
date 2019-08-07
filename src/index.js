d6281fc84b50159b80d3025f8a277cc3b0ae51fc
saikrishna6415 <saikrishnakotagiri16@gmail.com>
2019-08-07 21:58:15 +0530

    added music files

---
+++ src/index.js
@@ -0,0 +1,142 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const crazyfeeling = "./music/crazyfeeling.mp3";
+const Vaadi_ne_Vaadi = "./music/vaadi-nee-vaadi.mp3";
+const Meesaya_Murukku= "/music/Meesaya-Murukku.mp3" ;
+const Adiye_Sakkarakatti= "/music/Adiye-Sakkarakatti.mp3" ;
+const Maattikkichey_Maattikkichey = "/music/Maattikkichey-Maattikkichey.mp3";
+
+function getTime(time) {
+  if (!isNaN(time)) {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    selectedTrack: null,
+    player: "stopped",
+    currentTime: null,
+    duration: null
+  };
+
+  componentDidMount() {
+    this.player.addEventListener("timeupdate", e => {
+      this.setState({
+        currentTime: e.target.currentTime,
+        duration: e.target.duration
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.player.removeEventListener("timeupdate", () => {});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.selectedTrack !== prevState.selectedTrack) {
+      let track;
+      switch (this.state.selectedTrack) {
+        case "crazy feeling":
+          track = crazyfeeling;
+          break;
+        case "vaadi-Nee-vaadi":
+          track = Vaadi_ne_Vaadi;
+          break;
+        case "Meesaya-Murukku":
+          track = Meesaya_Murukku;
+          break;
+        case "Adiye-Sakkarakatti":
+          track = Adiye_Sakkarakatti;
+          break;
+        case "Maattikkichey-Maattikkichey":
+          track = Maattikkichey_Maattikkichey;
+          break;
+        default:
+          break;
+      }
+      if (track) {
+        this.player.src = track;
+        this.player.play();
+        this.setState({ player: "playing", duration: this.player.duration });
+      }
+    }
+    if (this.state.player !== prevState.player) {
+      if (this.state.player === "paused") {
+        this.player.pause();
+      } else if (this.state.player === "stopped") {
+        this.player.pause();
+        this.player.currentTime = 0;
+        this.setState({ selectedTrack: null });
+      } else if (
+        this.state.player === "playing" &&
+        prevState.player === "paused"
+      ) {
+        this.player.play();
+      }
+    }
+  }
+
+  render() {
+    const list = [
+      { id: 1, title: "crazy feeling" },
+      { id: 2, title: "Vaadi-Nee-Vaadi" },
+      { id: 3, title: "Meesaya-Murukku" },
+      { id: 4, title: "Adiye-Sakkarakatti" },
+      { id: 5, title: "Maattikkichey-Maattikkichey" },
+    ].map(item => {
+      return (
+        <li
+          key={item.id}
+          onClick={() => this.setState({ selectedTrack: item.title })}
+        >
+          {item.title}
+        </li>
+      );
+    });
+
+    const currentTime = getTime(this.state.currentTime);
+    const duration = getTime(this.state.duration);
+
+    return (
+      <>
+        <h1>My Music Player</h1>
+        <ul>{list}</ul>
+        <div>
+          {this.state.player === "paused" && (
+            <button onClick={() => this.setState({ player: "playing" })}>
+              Play
+            </button>
+          )}
+          {this.state.player === "playing" && (
+            <button onClick={() => this.setState({ player: "paused" })}>
+              Pause
+            </button>
+          )}
+          {this.state.player === "playing" || this.state.player === "paused" ? (
+            <button onClick={() => this.setState({ player: "stopped" })}>
+              Stop
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+        {this.state.player === "playing" || this.state.player === "paused" ? (
+          <div>
+            {currentTime} / {duration}
+          </div>
+        ) : (
+          ""
+        )}
+        <audio ref={ref => (this.player = ref)} />
+      </>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
